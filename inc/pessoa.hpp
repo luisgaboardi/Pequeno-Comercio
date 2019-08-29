0ccbaa81294dc85a82d1a790ed88d740aab5ac53
luisgaboardi <luis.ggaboardi@gmail.com>
2019-08-29 15:00:54 -0300

    Declaring constructor and methods in 'pessoa.hpp'

--- inc/pessoa.hpp
+++ inc/pessoa.hpp
@@ -12,5 +12,20 @@ class Pessoa
 				endereco;
 
 	public:
+		Pessoa();
+		~Pessoa();
+
+		string get_nome();
+		string get_email();
+		string get_cpf();
+		string get_telefone();
+		string get_endereco();
+
+		void set_nome(string nome);
+		void set_email(string email);
+		void set_cpf(string cpf);
+		void set_telefone(string telefone);
+		void set_endereco(string endereco);
+
 		void imprime_dados();
 };
