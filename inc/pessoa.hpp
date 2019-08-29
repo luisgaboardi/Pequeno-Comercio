c6926312cc1554bfe69837239c53ab9d73cacfe1
luisgaboardi <luis.ggaboardi@gmail.com>
2019-08-29 14:53:12 -0300

    Creating class 'Pessoa' and 'main.cpp'

---
+++ inc/pessoa.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Pessoa
+{
+	private:
+		string nome,
+				email,
+				cpf,
+				telefone,
+				endereco;
+
+	public:
+		void imprime_dados();
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <pessoa.hpp>
+
+int main()
+{
+	return 0;
+}
