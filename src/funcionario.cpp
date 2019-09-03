18207b3d8d475bacb73733e5da789861c2d65a14
luisggaboardi <luis.ggaboardi@gmail.com>
2019-09-03 09:49:24 -0300

    Developed class 'Funcionario.cpp' and included it in main

---
+++ src/funcionario.cpp
@@ -0,0 +1,34 @@
+#include "funcionario.hpp"
+
+Funcionario::Funcionario()
+{
+	this->salario = 0.0f;
+	this->trabalhando = false;
+}
+
+Funcionario::~Funcionario(){}
+
+float Funcionario::get_salario(){
+	return salario;
+}
+void Funcionario::set_salario(float salario){
+	this->salario = salario;
+}
+
+bool Funcionario::get_trabalhando(){
+	return trabalhando;
+}
+void Funcionario::set_trabalhando(bool trabalhando){
+	this->trabalhando = trabalhando;
+}
+
+void Funcionario::imprime_dados()
+{
+	cout << "Nome: " << get_nome() << endl;
+	cout << "CPF: " << get_cpf() << endl;
+	cout << "Email: " << get_email() << endl;
+	cout << "Telefone: " << get_telefone() << endl;
+	cout << "Endereço: " << get_endereco() << endl;
+	cout << "Salário: " << get_salario() << endl;
+	cout << "Trabalhando: " << get_trabalhando() << endl << endl;
+}
